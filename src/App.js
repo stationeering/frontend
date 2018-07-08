cee02da5b726707baa6b839da7c50b60f219b5a9
Melair <melair@caldonia.net>
2018-07-08 19:00:42 +0100

    Initial implementatioen of IC emulator with URL hash saving.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
 import React, { Component } from 'react';
 import ICSocket from './tools/ic/ICSocket';
+import { NavLink, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ICSocket />
+        <Header />
+        <Navigation />
+        <Main />
       </div>
+    );
+  }
+}
+
+class Header extends Component {
+  render() {
+    return (
+    <div className="header">
+      <h1>Stationeering</h1>
+      <p>An unoffical fan site for <a href="https://store.steampowered.com/app/544550/Stationeers/">Stationeers</a>!</p>
+    </div>
+    );
+  }
+}
+
+class Navigation extends Component {
+  render() {
+    return (
+    <div className="navigation">
+      <ul>
+        <li><NavLink to='/'>Homepage</NavLink></li>
+        <li><NavLink to='/tools/ic'>IC Simulation</NavLink></li>
+      </ul>
+    </div>
+    );
+  }
+}
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <p>Hello!</p>
+      </div>
+    )
+  }
+}
+
+class Main extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" Component={Home} />
+        <Route exact path="/tools/ic" component={ICSocket} />
+      </Switch>
     );
   }
 }
--- src/tools/ic/ICSocket.js
+++ src/tools/ic/ICSocket.js
@@ -5,10 +5,6 @@ import './ICSocket.css';
 class ICSocket extends Component {
   constructor(props) {
     super(props);
-    let defaultCode = "GT i0 75 r0     // Specify constants inline in code\nLT i1 353 r1    // Check temperature\nMAX r0 r1 r2    // Is temp too high or pressure too low?\nSEL r2 0 1 o0   // Set the door status\nSEL r2 0 4 o1   // Set the LED light (white/red)";
-
-    this.state = { ic: new IC(), program: defaultCode, errors: [] };    
-    this.state = Object.assign(this.transferICState(), this.state);
 
     this.programChange = this.programChange.bind(this);
     this.step = this.step.bind(this);
@@ -16,44 +12,127 @@ class ICSocket extends Component {
     this.restart = this.restart.bind(this);
     this.clearInternalRegisters = this.clearInternalRegisters.bind(this);
     this.setRegister = this.setRegister.bind(this);
+    this.toggleRunAfterRegisterChange = this.toggleRunAfterRegisterChange.bind(this);
+    this.hashChanged = this.hashChanged.bind(this);
+
+    let defaultCode = "ADD 1 r0 r0 // Increment r0.";
+
+    this.state = { ic: new IC(), program: defaultCode, errors: [], labels: { input: [], output: [], internal: [] }, runAfterRegisterChange: false, currentHash: "" };
+    this.loadProgram(defaultCode);
   }
 
   componentDidMount() {
-    this.loadProgram(this.state.program);
+    this.loadStateFromHash();
+    this.transferICState();
+
+    window.addEventListener("hashchange", this.hashChanged, false);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("hashchange", this.hashChanged, false);
+  }
+
+  componentDidUpdate() {
+    this.saveStateToHash();
+  }
+
+  hashChanged(event) {
+    this.loadStateFromHash();
+  }
+
+  loadStateFromHash() {
+    if (window.location.hash.length === 0) {
+      return {};
+    }
+
+    let rawHash = window.location.hash.substring(1);
+
+    if (rawHash === this.state.currentHash) {
+      return {};
+    }
+
+    let json = atob(rawHash);
+    let data = JSON.parse(json);
+
+    let returnData = { currentHash: rawHash };
+
+    if (data.hasOwnProperty("program")) {
+      returnData["program"] = data.program;
+      this.loadProgram(data.program);
+    }
+
+    if (data.hasOwnProperty("runAfterRegisterChange")) {
+      returnData["runAfterRegisterChange"] = data.runAfterRegisterChange;
+    }
+
+    if (data.hasOwnProperty("registers")) {
+      Object.keys(data.registers).forEach((key) => {
+        data.registers[key].forEach((value, i) => {
+          switch (key) {
+            case "input":
+              this.state.ic.setInputRegister(i, value);
+              break;
+            case "output":
+              this.state.ic.setOutputRegister(i, value);
+              break;
+            case "internal":
+              this.state.ic.setInternalRegister(i, value);
+              break;
+            default:
+          }
+        });
+      });
+    }
+
+    this.setState(returnData);
+  }
+
+  saveStateToHash() {
+    let data = { program: this.state.program, registers: { input: this.state.inputRegisters, output: this.state.outputRegisters, internal: this.state.internalRegisters }, runAfterRegisterChange: this.state.runAfterRegisterChange };
+    let json = JSON.stringify(data);
+    let base64 = btoa(json);
+
+    if (this.state.currentHash !== base64) {
+      window.location.hash = base64;
+      this.setState({ currentHash: base64 })
+    }
   }
 
   transferICState() {
     let ic = this.state.ic;
 
-    return {
+    this.setState({
       inputRegisters: ic.getInputRegisters(),
       outputRegisters: ic.getOutputRegisters(),
       internalRegisters: ic.getInternalRegisters(),
       programCounter: ic.programCounter(),
       instructionCount: ic.instructionCount()
-    };
+    });
   }
 
   render() {
-    var outOfSteps = this.state.ic.programCounter() >= this.state.ic.instructionCount() || this.state.errors.length > 0;
+    var canRun = this.state.ic.programCounter() >= this.state.ic.instructionCount() || this.state.errors.length > 0;
 
-    return (    
+    return (
       <div className="ICSocket">
         <h3>Input Registers</h3>
-        <Registers registers={this.state.inputRegisters} type="input" setRegister={this.setRegister} />
+        <Registers registers={this.state.inputRegisters} type="input" setRegister={this.setRegister} labels={this.state.labels.input} />
         <h3>Output Registers</h3>
-        <Registers registers={this.state.outputRegisters} type="output" setRegister={this.setRegister} />
+        <Registers registers={this.state.outputRegisters} type="output" setRegister={this.setRegister} labels={this.state.labels.output} />
         <h3>Internal Registers</h3>
-        <Registers registers={this.state.internalRegisters} type="internal" setRegister={this.setRegister} />
+        <Registers registers={this.state.internalRegisters} type="internal" setRegister={this.setRegister} labels={this.state.labels.internal} />
         <h3>Program</h3>
-        <textarea rows="15" cols="80" defaultValue={this.state.program} onChange={this.programChange}/>
+        <textarea rows="15" cols="80" value={this.state.program} onChange={this.programChange} />
         <ProgramErrors errors={this.state.errors} />
         <h3>Control</h3>
         <div>Program Counter: {this.state.programCounter} Total Instructions: {this.state.instructionCount}</div>
-        <button onClick={this.step} disabled={outOfSteps}>Step</button>
-        <button onClick={this.run} disabled={outOfSteps}>Run</button>
-        <button onClick={this.restart}>Restart</button>
-        <button onClick={this.clearInternalRegisters}>Clear Internal Registers</button>
+        <div>
+          <button onClick={this.step} disabled={canRun}>Step</button>
+          <button onClick={this.run} disabled={canRun}>Run</button>
+          <button onClick={this.restart}>Restart</button>
+          <button onClick={this.clearInternalRegisters}>Clear Internal Registers</button>
+        </div>
+        Auto Run After Register Change <input type="checkbox" checked={this.state.runAfterRegisterChange} onChange={this.toggleRunAfterRegisterChange} />
       </div>
     );
   }
@@ -89,39 +168,74 @@ class ICSocket extends Component {
   }
 
   setRegister(type, index, value) {
-    switch(type) {
+    switch (type) {
       case "input":
-      this.state.ic.setInputRegister(index, value);
-      break;
+        this.state.ic.setInputRegister(index, value);
+        break;
       case "output":
-      this.state.ic.setOutputRegister(index, value);
-      break;
+        this.state.ic.setOutputRegister(index, value);
+        break;
       case "internal":
-      this.state.ic.setInternalRegister(index, value);
-      break;
+        this.state.ic.setInternalRegister(index, value);
+        break;
       default:
     }
-    this.setState(this.transferICState());
+
+    this.transferICState();
+
+    if (this.state.runAfterRegisterChange) {
+      this.run();
+    }
   }
 
   programChange(event) {
+    this.setState({ program: event.target.value });
     this.loadProgram(event.target.value);
   }
 
   loadProgram(program) {
-    let ic = this.state.ic;    
-    this.setState({ errors: ic.load(program) });  
+    let ic = this.state.ic;
+    this.setState({ errors: ic.load(program) });
     this.setState({ instructionCount: ic.instructionCount() });
+    this.parseLabels(program);
+  }
+
+  parseLabels(program) {
+    let labels = { input: [], output: [], internal: [] };
+    let lines = program.split("\n")
+
+    lines.forEach((line, i) => {
+      if (line.startsWith("//:")) {
+        let tokens = line.split(":");
+
+        if (tokens.length === 4) {
+          if (labels.hasOwnProperty(tokens[1])) {
+            let number = Number.parseInt(tokens[2], 10);
+            labels[tokens[1]][number] = tokens[3];
+          }
+        }
+      }
+    });
+
+    this.setState({ labels });
+  }
+
+  toggleRunAfterRegisterChange(event) {
+    this.setState({ runAfterRegisterChange: event.target.checked });
   }
 }
 
 class ProgramErrors extends Component {
   render() {
-    return (
-      <div className="programErrors">
-        {this.renderErrors(this.props.errors)}
-      </div>
-    );
+    if (this.props.errors.length > 0) {
+      return (
+        <ul className="programErrors">
+          {this.renderErrors(this.props.errors)}
+        </ul>
+      );
+    } else {
+      return null;
+    }
   }
 
   renderErrors(errors) {
@@ -133,25 +247,52 @@ class ProgramError extends Component {
   render() {
     let line = this.props.error.line;
     let error = this.props.error.error;
-    let field = this.props.error.field ? `(Field ${this.props.error.field})` : "";
+    let field = this.props.error.field ? ` / Field ${this.props.error.field}` : "";
+    let errorDescription = this.lookUpError(error);
 
     return (
-      <div className="programError">Line {line}: {error} {field}</div>
+      <li className="programError">Line {line}{field}: {errorDescription} ({error}) </li>
     );
+  }
+
+  lookUpError(error) {
+    switch (error) {
+      case "TOO_MANY_INSTRUCTIONS":
+        return "There are too many instructions present in your program, there is a limit on the number which an IC can take.";
+      case "FIELD_COUNT_MISMATCH":
+        return "The instruction you are using requires a different number of fields then you have provided.";
+      case "READ_ONLY_REGISTER":
+        return "The register you are outputting a value to is read only (or a litteral), choose an output or internal register.";
+      case "OUT_OF_BOUND_REGISTER":
+        return "The register number you have specified does not exist.";
+      case "UNKNOWN_REGISTER":
+        return "The register prefix you have provided is unknown, use i, o or r."
+      case "UNKNOWN_INSTRUCTION":
+        return "The instruction you have specified does not exist, check the spelling."
+      default:
+        return "Unknown error."
+    }
   }
 }
 
 class Registers extends Component {
   render() {
     return (
-      <div className="registers">
-        {this.renderRegisters()}
-      </div>
+      <table className="registers">
+        <thead>
+          <tr><th>Index</th><th>Set</th><th>Value</th><th>Label</th></tr>
+        </thead>
+        <tbody>
+          {this.renderRegisters()}
+        </tbody>
+      </table>
     );
   }
 
   renderRegisters() {
-      return this.props.registers.map((value, i) => <Register key={i} index={i} value={value} type={this.props.type} setRegister={this.props.setRegister} />);
+    if (this.props.registers) {
+      return this.props.registers.map((value, i) => <Register key={i} index={i} value={value} label={this.props.labels[i]} type={this.props.type} setRegister={this.props.setRegister} />);
+    }
   }
 }
 
@@ -159,26 +300,41 @@ class Register extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { inputValue: "0" };
+    this.state = { inputValue: "" };
 
     this.onClick = this.onClick.bind(this);
     this.onChange = this.onChange.bind(this);
+    this.onKeyPress = this.onKeyPress.bind(this);
   }
 
   render() {
     return (
-      <div className="register">
-        {this.props.index}: New: <input value={this.state.inputValue} type="text" size="5" onChange={this.onChange} /> <button onClick={this.onClick}>-></button> Current: {this.props.value}
-      </div>
+      <tr className="register">
+        <td>{this.props.index}</td>
+        <td><input type="text" size="5" onChange={this.onChange} onKeyPress={this.onKeyPress} value={this.state.inputValue} /> <button onClick={this.onClick}>-&gt;</button></td>
+        <td>{this.props.value}</td>
+        <td>{this.props.label}</td>
+      </tr>
     );
   }
 
   onChange(event) {
-    this.setState({ inputValue: Number.parseFloat(event.target.value) });
+    this.setState({ inputValue: event.target.value });
   }
 
-  onClick(event) {
-    this.props.setRegister(this.props.type, this.props.index, this.state.inputValue);
+  onClick() {
+    var newVal = Number.parseFloat(this.state.inputValue);
+
+    if (!Number.isNaN(newVal)) {
+      this.props.setRegister(this.props.type, this.props.index, Number.parseFloat(this.state.inputValue));
+      this.setState({ inputValue: "" });
+    }
+  }
+
+  onKeyPress(event) {
+    if (event.key === "Enter") {
+      this.onClick();
+    }
   }
 }
 
