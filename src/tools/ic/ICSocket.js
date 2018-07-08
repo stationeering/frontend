b4cb06ea36e22ae5b588c13dbacb961cc62b3a96
Melair <melair@caldonia.net>
2018-07-08 10:46:29 +0100

    Tidy up default app, bringing in IC for testing.

---
+++ src/tools/ic/ICSocket.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './ICSocket.css';
+
+class ICSocket extends Component {
+  render() {
+    return (
+      <div className="ICSocket">
+        
+      </div>
+    );
+  }
+}
+
+export default ICSocket;
