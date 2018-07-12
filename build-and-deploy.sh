dea69d54c9639d68478fc8db198e1ad655ce90a0
Melair <melair@caldonia.net>
2018-07-12 14:45:36 +0100

    Update to new buckets.

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 npm run-script build
-AWS_PROFILE=deploy aws s3 sync build/. s3://stationeering.com/ --delete --cache-control max-age=31536000,public
+AWS_PROFILE=deploy aws s3 sync build/. s3://stationeering-app/ --delete --cache-control max-age=31536000,public
 
 ROOT_FILES=`find build/ -maxdepth 1 -type f`
 
 for file in $ROOT_FILES; do
-AWS_PROFILE=deploy aws s3 cp --cache-control max-age=120,no-cache,no-store,must-revalidate $file s3://stationeering.com/
+AWS_PROFILE=deploy aws s3 cp --cache-control max-age=120,no-cache,no-store,must-revalidate $file s3://stationeering-app/
 done
 
-AWS_PROFILE=deploy aws cloudfront create-invalidation --distribution-id=EC6V6Y638O5OD --paths="/*"
+AWS_PROFILE=deploy aws cloudfront create-invalidation --distribution-id=ETYAB7ZW9G4WW --paths="/*"
