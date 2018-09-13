9fc3cac3d29a322396af7331143cd057dfccb9e7
Melair <melair@caldonia.net>
2018-09-13 15:40:09 +0100

    Format the instruction table nicer.

--- src/tools/ic/ICInstructionSet.js
+++ src/tools/ic/ICInstructionSet.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Panel } from 'react-bootstrap';
+import { Panel, Table } from 'react-bootstrap';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -11,78 +11,28 @@ class ICInstructionSet extends Component {
     render() {
         return (<Panel>
             <Panel.Heading>
-              <Panel.Title componentClass="h3"><FontAwesomeIcon icon="book" /> Stationeers Instruction Set</Panel.Title>
+                <Panel.Title componentClass="h3"><FontAwesomeIcon icon="book" /> Stationeers Instruction Set</Panel.Title>
             </Panel.Heading>
             <Panel.Body>
-            <pre>{`# Text after a # will be ignored to the end of the line. The amount of white
-# space between arguments isn't important, but new lines start a new command.
-
-# In the instructions fields can only take certain types, these are:
-# - d is a register
-# - s and t are registers, or floats
-# - a is a non-negative integer value              
-# - i is an IO register
-# - f is a setting on an external device
-
-move    d s     # stores the value of s in d
-
-add     d s t   # calculates s + t and stores the result in d
-sub     d s t   # calculates s - t and stores the result in d
-mul     d s t   # calculates s * t and stores the result in d
-div     d s t   # calculates s / t and stores the result in d
-mod     d s t   # calculates s mod t and stores the result in d. Note this
-              # doesn't behave like the % operator - the result will be
-              # positive even if the either of the operands are negative
-
-slt     d s t   # stores 1 in d if s < t, 0 otherwise
-
-sqrt    d s     # calculates sqrt(s) and stores the result in d
-round   d s     # finds the rounded value of s and stores the result in d
-trunc   d s     # finds the truncated value of s and stores the result in d
-ceil    d s     # calculates the ceiling of s and stores the result in d
-floor   d s     # calculates the floor of s and stores the result in d
-
-max     d s t   # calculates the maximum of s and t and stores the result in d
-min     d s t   # calculates the minimum of s and t and stores the result in d
-abs     d s     # calculates the absolute value of s and stores the result in d
-log     d s     # calculates the natural logarithm of s and stores the result
-              # in d
-exp     d s     # calculates the exponential of s and stores the result in d
-rand    d       # selects a random number uniformly at random between 0 and 1
-              # inclusive and stores the result in d
-
-# boolean arithmetic uses the C convention that 0 is false and any non-zero
-# value is true.
-and     d s t   # stores 1 in d if both s and t have non-zero values,
-              # 0 otherwise
-or      d s t   # stores 1 in d if either s or t have non-zero values,
-              # 0 otherwise
-xor     d s t   # stores 1 in d if exactly one of s and t are non-zero,
-              # 0 otherwise
-nor     d s t   # stores 1 in d if both s and t equal zero, 0 otherwise
-
-# Lines are numbered starting at zero
-j             a # jumps to line a.
-bltz      s   a # jumps to line a if s <  0
-blez      s   a # jumps to line a if s <= 0
-bgez      s   a # jumps to line a if s >= 0
-bgtz      s   a # jumps to line a if s >  0
-beq       s t a # jumps to line a if s == t
-bne       s t a # jumps to line a if s != t
-
-# Interacting with external devices
-
-s         i f s # Save the contents of s to the device on IO i, variable f.
-l         d i f # Load the contents of device on IO i, variable f, and write to register d.
-
-# For example, if d0 and d1 are LEDs, copying a color from d0 to d1.
-#
-# l r0 d0 Color # Load the current color of d0 into register 0.
-# s d1 Color r0 # Save value from register 0 into d0 color.
-
-yield           # ceases code execution for this power tick`}</pre>
+                <p>
+                    Comments are made by using the <code>#</code> symbol, either at the start of a line or after an instruction.
+                </p>
+            <Table condensed>
+            <thead>
+<tr><th>Instruction</th><th>Field 1</th><th>Field 2</th><th>Field 3</th><th>Description</th></tr></thead>
+<tbody><tr><td>move</td><td><code>d</code></td><td><code>s</code></td><td>&nbsp;</td><td>Store the value of <code>s</code> in <code>d</code>.</td></tr><tr><td>add</td><td><code>d</code></td><td><code>s</code></td><td><code>t</code></td><td>Add <code>s</code> and <code>t</code> together, store in <code>d</code>.</td></tr><tr><td>sub</td><td><code>d</code></td><td><code>s</code></td><td><code>t</code></td><td>Subtract <code>t</code> from <code>s</code>, store in <code>d</code>.</td></tr><tr><td>mul</td><td><code>d</code></td><td><code>s</code></td><td><code>t</code></td><td>Multiply <code>s</code> and <code>t</code>, store in <code>d</code>.</td></tr><tr><td>div</td><td><code>d</code></td><td><code>s</code></td><td><code>t</code></td><td>Divide <code>s</code> by <code>t</code>, store in <code>d</code>.</td></tr><tr><td>mod</td><td><code>d</code></td><td><code>s</code></td><td><code>t</code></td><td>Modulus <code>s</code> by <code>t</code>, store in <code>d</code>.<br /><strong>Always positive value.</strong></td></tr><tr><td>slt</td><td><code>d</code></td><td><code>s</code></td><td><code>t</code></td><td>Store <code>1</code> in <code>d</code> if <code>s</code> &lt; <code>d</code>, if not store <code>0</code> in <code>d</code>.</td></tr><tr><td>sqrt</td><td><code>d</code></td><td><code>s</code></td><td>&nbsp;</td><td>Square root <code>s</code> and store in <code>d</code>.</td></tr><tr><td>round</td><td><code>d</code></td><td><code>s</code></td><td>&nbsp;</td><td>Rounds <code>s</code> and store in <code>d</code>.</td></tr><tr><td>trunc</td><td><code>d</code></td><td><code>s</code></td><td>&nbsp;</td><td>Truncate <code>s</code> and store in <code>d</code>.</td></tr><tr><td>ceil</td><td><code>d</code></td><td><code>s</code></td><td>&nbsp;</td><td>Ceiling of <code>s</code> and store in <code>d</code>.</td></tr><tr><td>floor</td><td><code>d</code></td><td><code>s</code></td><td>&nbsp;</td><td>Floor of <code>s</code> and store in <code>d</code>.</td></tr><tr><td>max</td><td><code>d</code></td><td><code>s</code></td><td><code>t</code></td><td>Find the higher value of <code>s</code> and <code>t</code>, store in <code>d</code>.</td></tr><tr><td>min</td><td><code>d</code></td><td><code>s</code></td><td><code>t</code></td><td>Find the lower value of <code>s</code> and <code>t</code>, store in <code>d</code>.</td></tr><tr><td>abs</td><td><code>d</code></td><td><code>s</code></td><td>&nbsp;</td><td>Calculate absolute value of <code>s</code>, store in <code>d</code>.</td></tr><tr><td>log</td><td><code>d</code></td><td><code>s</code></td><td>&nbsp;</td><td>Calculate natural log of <code>s</code>, store in <code>d</code>.</td></tr><tr><td>exp</td><td><code>d</code></td><td><code>s</code></td><td>&nbsp;</td><td>Calculate exponential of <code>s</code>, store in <code>d</code>.</td></tr><tr><td>rand</td><td><code>d</code></td><td>&nbsp;</td><td>&nbsp;</td><td>Random number between 0.0-1.0, store in <code>d</code>.</td></tr><tr><td>and</td><td><code>d</code></td><td><code>s</code></td><td><code>t</code></td><td>Store <code>1</code> in <code>d</code>, if both <code>s</code> and <code>t</code> are non zero.</td></tr><tr><td>or</td><td><code>d</code></td><td><code>s</code></td><td><code>t</code></td><td>Store <code>1</code> in <code>d</code>, if either <code>s</code> or <code>t</code> are non zero.</td></tr><tr><td>xor</td><td><code>d</code></td><td><code>s</code></td><td><code>t</code></td><td>Store <code>1</code> in <code>d</code>, if only one of <code>s</code> or <code>t</code> is non zero.</td></tr><tr><td>nor</td><td><code>d</code></td><td><code>s</code></td><td><code>t</code></td><td>Store <code>1</code> in <code>d</code>, if both <code>s</code> and <code>t</code> are both zero.</td></tr><tr><td>j</td><td><code>a</code></td><td>&nbsp;</td><td>&nbsp;</td><td>Jump program to <code>a</code>.</td></tr><tr><td>bltz</td><td><code>s</code></td><td><code>a</code></td><td>&nbsp;</td><td>Jump program to <code>a</code>, if <code>s</code> is &lt; 0.</td></tr><tr><td>blez</td><td><code>s</code></td><td><code>a</code></td><td>&nbsp;</td><td>Jump program to <code>a</code>, if <code>s</code> is &lt;= 0.</td></tr><tr><td>bgez</td><td><code>s</code></td><td><code>a</code></td><td>&nbsp;</td><td>Jump program to <code>a</code>, if <code>s</code> is &gt;= 0.</td></tr><tr><td>bgtz</td><td><code>s</code></td><td><code>a</code></td><td>&nbsp;</td><td>Jump program to <code>a</code>, if <code>s</code> is &gt; 0.</td></tr><tr><td>beq</td><td><code>s</code></td><td><code>t</code></td><td><code>a</code></td><td>Jump program to <code>a</code>, if <code>s</code> == <code>t</code>.</td></tr><tr><td>bne</td><td><code>s</code></td><td><code>t</code></td><td><code>a</code></td><td>Jump program to <code>a</code>, if <code>s</code> != <code>t</code>.</td></tr><tr><td>yield</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>Yield processor for this power tick.</td></tr><tr><td>s</td><td><code>i</code></td><td><code>f</code></td><td><code>s</code></td><td>Save <code>s</code> into IO port <code>i</code>, variable <code>f</code>.</td></tr><tr><td>l</td><td><code>d</code></td><td><code>i</code></td><td><code>f</code></td><td>Load IO port <code>i</code>, variable <code>f</code> into <code>d</code>.</td></tr></tbody>
+            </Table>
+                <h4>Key</h4>
+                <ul>
+                    <li><code>d</code> - An internal register (r0 - r15)</li>
+                    <li><code>s</code> - An internal register (r0 - r15), a constant integer or a constant float.</li>
+                    <li><code>t</code> - An internal register (r0 - r15), a constant integer or a constant float.</li>
+                    <li><code>a</code> - An address within the program, always a positive integer.</li>
+                    <li><code>i</code> - Device register (d0 - d5).</li>
+                    <li><code>f</code> - String variable name available on the preceeding device.</li>
+                </ul>
             </Panel.Body>
-          </Panel>);
+        </Panel>);
     }
 }
 
