38db2f72c5670161fea251818cfa0de7e4b1d8cc
Melair <melair@caldonia.net>
2018-11-27 11:31:55 +0000

    Add index list of things, with attributes and made/constructed by.

--- src/guide/Guide.js
+++ src/guide/Guide.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
-import { Row, Col, Label, Panel } from 'react-bootstrap';
+import { Row, Col, Label, Panel, Table } from 'react-bootstrap';
 
 import axios from 'axios';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faSpinner, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import { faSpinner, faTimesCircle, faHandHolding, faWrench, faMicrochip, faSprayCan, faUtensils, faLeaf, faUserAstronaut, faBug, faIndustry, faChessBoard, faBoxOpen } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faSpinner, faTimesCircle);
+library.add(faSpinner, faTimesCircle, faHandHolding, faWrench, faMicrochip, faLeaf, faSprayCan, faUtensils, faUserAstronaut, faBug, faIndustry, faChessBoard, faBoxOpen);
+
+const MANUFACTORY_TO_THING = {
+  "FabricatorRecipes": "StructureFabricator",
+  "ElectronicsPrinterRecipes": "StructureElectronicsPrinter",
+  "OrganicsPrinterRecipes": "StructureOrganicsPrinter",
+  "ChemistryRecipes": "ApplianceChemistryStation",
+  "MicrowaveRecipes": "ApplianceMicrowave",
+  "ToolManufactoryRecipes": "StructureToolManufactory",
+  "HydraulicPipeBenderRecipes": "StructureHydraulicPipeBender",
+  "SecurityPrinterRecipes": "StructureSecurityPrinter",
+  "FurnaceRecipes": "StructureFurnace",
+  "ArcFurnaceRecipes": "StructureArcFurnace",
+  "AutolatheRecipes": "StructureAutolathe",
+  "CentrifugeRecipes": "StructureCentrifuge",
+  "PaintMixRecipes": "AppliancePaintMixer"
+}
 
 class Guide extends Component {
   constructor(props) {
@@ -59,7 +75,8 @@ class Guide extends Component {
 
     this.loadData(`https://data.stationeering.com/recipes/${branch}.json`,
     (t, data) => {
-      t.setState({ data: { ...this.state.data, recipes: data }, loading: { ...this.state.loading, recipes: { state: "success" } }});
+      var transformedData = t.transformRecipeData(data.recipes);
+      t.setState({ data: { ...this.state.data, recipes: transformedData }, loading: { ...this.state.loading, recipes: { state: "success" } }});
     },
     (t, msg) => {
       t.setState({ loading: { ...this.state.loading, recipes: { state: "failed", message: msg } }});
@@ -74,6 +91,26 @@ class Guide extends Component {
       t.setState({ loading: { ...this.state.loading, scenarios: { state: "failed", message: msg } }});
     },
     this);
+  }
+
+  transformRecipeData(recipes) {
+    var transformed = {};
+
+    for (var recipe of recipes) {
+      if (recipe.manufactory === "IngotRecipes") {
+        continue;
+      }
+
+      if (!Object.keys(transformed).includes(recipe.item)) {
+        transformed[recipe.item] = {};
+      }
+
+      var remappedManufactory = MANUFACTORY_TO_THING[recipe.manufactory] || recipe.manufactory;
+
+      transformed[recipe.item][remappedManufactory] = recipe.ingredients;
+    }
+
+    return transformed;
   }
 
   loadData(url, success, failure, t) {
@@ -99,7 +136,7 @@ class Guide extends Component {
       <div>
         <Row>
           <Col md={12}>
-            <h3>Stationeering's Guide to Stationeers</h3>
+            <h3>Stationeering's Guide to Stationeers ({this.state.data.desired} branch)</h3>
             {isLoading && <LoadingNotice states={this.state.loading} />}
             {!isLoading && <GuideContent states={this.state} />}
           </Col>
@@ -146,19 +183,79 @@ class LoadState extends Component {
 
 class GuideContent extends Component {
   render() {
-    var items = Object.keys(this.props.states.data.things).map((t) => <li>{t}</li>)
-
     return(
       <Row>
-        <Col md={6}>
-          <h3>Things</h3>
-          <ul>
-            {items}
-          </ul>
+        <Col md={12}>
+          <h4>Things</h4>
+          <ThingList things={this.props.states.data.things} recipes={this.props.states.data.recipes} language={this.props.states.language.mapping.sections.Things} />
         </Col>
       </Row>
     );
   }
 }
+
+class ThingList extends Component {
+  render() {
+    var thingKeys = Object.keys(this.props.things);
+
+    return (
+      <Table condensed>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Prefab (Hash)</th>
+            <th colSpan={11}>Attributes</th>
+            <th>Made/Constructed By</th>
+          </tr>
+        </thead>
+        <tbody>
+          {thingKeys.map((key) => <ThingListItem key={key} prefab={key} thing={this.props.things[key]} recipes={this.props.recipes[key]} language={this.props.language} />)}
+        </tbody>
+      </Table>
+    );
+  }
+}
+
+class ThingListItem extends Component {
+  render() {    
+    var madeBy = Object.keys(this.props.recipes || {}).sort().map((manufactory) => this.props.language[manufactory]);
+    var constructedBy = (this.props.thing.constructedBy || []).sort().map((kit) => this.props.language[kit]);
+
+    var output = [].concat(madeBy);
+    output = output.concat(constructedBy);
+
+    return (
+      <tr>
+        <td>{this.props.language[this.props.prefab] || this.props.prefab}</td>
+        <td><small className="text-info">{this.props.prefab} ({this.props.thing.prefabHash})</small></td>
+        <ThingFlag flag="item" icon="hand-holding" flags={this.props.thing.flags} title='Item' />
+        <ThingFlag flag="tool" icon="wrench" flags={this.props.thing.flags} title='Tool' />
+        <ThingFlag flag="constructor" icon="box-open" flags={this.props.thing.flags} title='Constructs A Structure' />
+        <ThingFlag flag="structure" icon="industry" flags={this.props.thing.flags} title='Grid Structure'/>
+        <ThingFlag flag="smallGrid" icon="chess-board" flags={this.props.thing.flags} title='Small Grid Structure' />
+        <ThingFlag flag="logicable" icon="microchip" flags={this.props.thing.flags} title='Has Logic Data'/>
+        <ThingFlag flag="plant" icon="leaf" flags={this.props.thing.flags} title='Plant' />
+        <ThingFlag flag="edible" icon="utensils" flags={this.props.thing.flags} title='Edible' />
+        <ThingFlag flag="paintable" icon="spray-can" flags={this.props.thing.flags} title='Paintable' />
+        <ThingFlag flag="entity" icon="bug" flags={this.props.thing.flags} title='Entity' />
+        <ThingFlag flag="npc" icon="user-astronaut" flags={this.props.thing.flags} title='NPC Entity with AI' />
+        <td>{output.join(", ")}</td>
+      </tr>
+    );
+  }
+}
+
+class ThingFlag extends Component {
+  render() {
+    if (!this.props.flags[this.props.flag]) {
+      return (<td />);
+    }
+
+    return (<td>
+      <abbr title={this.props.title}><FontAwesomeIcon icon={this.props.icon} /></abbr>
+    </td>)
+  }
+}
+
 
 export default Guide;
