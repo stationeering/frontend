8ef2453b745b04595261368a45a36ca31e6c5c32
Melair <melair@caldonia.net>
2018-11-29 21:28:29 +0000

    Readd registration to new service worker.

--- src/index.js
+++ src/index.js
@@ -12,4 +12,7 @@ ReactDOM.render(<BrowserRouter><App updateProxy={updateProxy} /></BrowserRouter>
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register({ onUpdate: function(registration) {
+  registration.skipWaiting();
+  updateProxy.notify();
+}});
