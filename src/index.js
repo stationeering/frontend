8869c171cac1a24e7ddb6aee1df2d46ab5c8ee51
Melair <melair@caldonia.net>
2018-11-29 21:18:47 +0000

    Clean up react project upgrading to the latest version of create-react-app, remove legacy test, fix permissions. Temporarily disable service worker.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import UpdateProxy from './UpdateProxy';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
 var updateProxy = new UpdateProxy();
 ReactDOM.render(<BrowserRouter><App updateProxy={updateProxy} /></BrowserRouter>, document.getElementById('root'));
-registerServiceWorker(updateProxy.notify);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
